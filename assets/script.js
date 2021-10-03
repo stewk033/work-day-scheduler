24304d2f4895b94c3fda0ba906d9199dd9baf4b8
stewk033 <kevin.c.stewart04@gmail.com>
2021-10-03 13:02:58 -0400

    add save button column

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
    $(document).ready(function() {
-    // Display date at top of page
+    // Display current date at top of page
     $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
     var container = $(".container");
@@ -9,16 +9,16 @@
     var timeSlot = function(hour) {
         var unit;
         var displayHour;
-        hour < 12 ? (unit = 'AM') : (unit = 'PM');
+        hour < 12 ? (unit = "AM") : (unit = "PM");
         hour > 12 ? (displayHour = hour - 12) : (displayHour = hour);
 
-        var hourColumn = $('<div>')
-            .addClass('col-1 hour')
+        var hourColumn = $("<div>")
+            .addClass("col-1 hour")
             .text(displayHour + unit);
 
         return hourColumn;
     };
-
+    // function to create text fields and assign colors
     var textSlot = function(hour) {
         descriptions = JSON.parse(localStorage.getItem("descriptions")) || new Array(9);
     
@@ -42,14 +42,21 @@
 
         return textColumn;
     }
+    // creates save column and places save button
+    var saveSlot = function(hour) {
+        var saveColumn = $("<div>").addClass("col-1 saveBtn").html(`<i class='fas fa-save' data-hour = ${hour}></i>`);
+        
+        return saveColumn;
+    }
 
     // creates rows and attaches time slot
     var createRow = function(hour) {
         var div = $('<div>').addClass('row');
         var hourColumn = timeSlot(hour);
         var textColumn = textSlot(hour);
+        var saveColumn = saveSlot(hour);
     
-        div.append(hourColumn).append(textColumn);
+        div.append(hourColumn).append(textColumn).append(saveColumn);
 
         return div;
     };
