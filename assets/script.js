cd0f030b80589b1bee919d1b7df3234cc2c12d94
stewk033 <kevin.c.stewart04@gmail.com>
2021-10-03 11:17:33 -0400

    add time column

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,37 @@
-$(document).ready(function () {
+   $(document).ready(function() {
     // Display date at top of page
     $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
+    var container = $(".container");
+
+    // function to create time column
+    var timeSlot = function(hour) {
+        var unit;
+        var displayHour;
+        hour < 12 ? (unit = ':00AM') : (unit = ':00PM');
+        hour > 12 ? (displayHour = hour - 12) : (displayHour = hour);
+
+        var hourColumn = $('<div>')
+            .addClass('col-1 hour')
+            .text(displayHour + unit);
+
+        return hourColumn;
+    };
+
+    var createRow = function(hour) {
+        var div = $('<div>').addClass('row');
+        var hourColumn = timeSlot(hour);
+    
+        div.append(hourColumn);
+        return div;
+    };
+
+    var timeView = function() {
+        for (var hour = 9; hour <= 17; hour++) {
+            container.append(createRow(hour));
+        }
+    };
+
+    timeView();
 });
 
